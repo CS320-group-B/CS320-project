6ddd1f4038465eed105889cd49a838f875aa1bbe
LeVi4869 <thanhvinhle3900@gmail.com>
2023-03-28 13:02:48 -0300

    general outline for homepage

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar () {
+    return (
+        <div>
+          <ul id="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">My Schedules</a></li>
+            <li><a href="#">Profile</a></li>
+          </ul>
+        </div>
+    );
+}
+
+export default Navbar
