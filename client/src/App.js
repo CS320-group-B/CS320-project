833dce862f3f167e2e7f919ff29b65f247d1037b
LeVi4869 <thanhvinhle3900@gmail.com>
2023-03-28 14:41:04 -0300

    Setup a page placeholder as a profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home"
+import Profile from "./components/Profile/Profile"
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-      <header className="App-header">
-        <p>
-          Welcome to Major Planner, an app to help you plan your course schedules
-        </p>
-      </header>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ function Navbar () {
     return (
         <div>
           <ul id="nav">
-            <li><a href="#">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="#">My Schedules</a></li>
-            <li><a href="#">Profile</a></li>
+            <li><a href="/Profile">Profile</a></li>
           </ul>
         </div>
     );
