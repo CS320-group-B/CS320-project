578af0c415d2a257302da33c8f2d10a0498111d1
flonga35 <dzl6699@gmail.com>
2023-04-11 09:31:37 -0400

    Added server code for API testing

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const courseRoutes = require('./routes/courses.js');
+const userRoutes = require('./routes/users.js');
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/course', courseRoutes);
+app.use('/user', userRoutes);
+
+const CONNECTION_URL = "mongodb+srv://user:123@cluster0.7sb85e6.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+     .catch((error) => console.log(error.message));
+
