342a9fecb94e0b41d61a272e4466b2e8e65b962a
danfuz <danryafuz@gmail.com>
2023-04-20 09:15:04 -0400

    Removed unsafe method

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,15 +2,15 @@ const express = require('express');
 const auth = require('../middleware/auth.js');
 const updPassword = require('../middleware/updatePassword.js')
 
-const { getUsers, addUser, updateUser, deleteUser, signin, signup } = require('../controllers/users.js');
+const { getUser, addUser, updateUser, deleteUser, signin, signup } = require('../controllers/users.js');
 
 const router = express.Router();
 
 
 router.post('/signin', signin);
 router.post('/signup', signup);
-router.get('/', getUsers);
-router.post('/', addUser);
+router.get('/:id', auth, getUser);
+router.post('/', auth, addUser);
 router.put('/:id', auth, updPassword, updateUser);
 router.delete('/:id', auth, deleteUser);
 
