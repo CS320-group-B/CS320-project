2e096c205a2c2e4366a12149dcbf52ca133918f4
flonga35 <dzl6699@gmail.com>
2023-04-25 12:48:41 -0400

    Refactor code

--- server/models/schedule_generator.js
+++ server/models/schedule_generator.js
@@ -2,45 +2,43 @@ const { Graph, alg } = require('graphlib');
 const { data } = require('./data');
 
 const courses = data.data;
-// Create an empty directed graph
-const graph = new Graph({ directed: true });
 
-// Add nodes to the graph for each course
-for (const course of courses) {
-  graph.setNode(course.key, course);
-}
+function createGraph(courses) {
+  // Create an empty directed graph
+  const graph = new Graph({ directed: true });
+  
+  // Add nodes to the graph for each course
+  for (const course of courses) {
+    graph.setNode(course.key, course);
+  }
 
-// Add edges to the graph for each prerequisite relationship
-for (const course of courses) {
-  const { key, prerequisites } = course;
-  if (prerequisites.length == 0) continue;
-  for (let i = 0; i < prerequisites.length; i++) {
-    for (const prerequisite of prerequisites[i].options) {
-      //if (!courseKeys.includes(prerequisite)) console.log(prerequisite);
-      graph.setEdge(prerequisite, key);
+  // Add edges to the graph for each prerequisite relationship
+  for (const course of courses) {
+    const { key, prerequisites } = course;
+    if (prerequisites.length == 0) continue;
+    for (let i = 0; i < prerequisites.length; i++) {
+      for (const prerequisite of prerequisites[i].options) {
+        //if (!courseKeys.includes(prerequisite)) console.log(prerequisite);
+        graph.setEdge(prerequisite, key);
+      }
     }
   }
-}
 
-// Add nodes that are not in data.js but are prerequisites
-for (const node of graph.nodes()) {
-  if (graph.node(node) == undefined) {
-    graph.setNode(node, { key: node, credits: 4})
+  // Add nodes that are not in data.js but are prerequisites
+  for (const node of graph.nodes()) {
+    if (graph.node(node) == undefined) {
+      graph.setNode(node, { key: node, credits: 4})
+    }
   }
+
+  return graph;
 }
 
-// Print the graph
+const graph = createGraph(courses);
 // console.log(json.write(graph)); 
-function printGraph(graph) {
-  for (const edge of graph.edges()) {
-    const v = edge.v.split(" ")[0] + "_" + edge.v.split(" ")[1];
-    const w = edge.w.split(" ")[0] + "_" + edge.w.split(" ")[1];
-    console.log( v + " -> " + w + ";");
-  }
-}
-// printGraph(graph);
 
-function printSubGraph(graph, key) {
+// make graph all ancestors and descendants of a node
+function makeSubGraph(graph, key) {
   if (!graph.hasNode(key)) { console.log(`Graph does not contain node: ${key}`); return; }
   // Get the set of nodes that should be included in the graph
   const nodes = new Set([key]);
@@ -75,12 +73,21 @@ function printSubGraph(graph, key) {
       subgraph.setEdge(edge.v, edge.w);
     }
   }
-  // Print the subgraph
-  printGraph(subgraph);
+  // return the subgraph
+  return subgraph;
   // console.log(subgraph.edges());
 }
 
-printSubGraph(graph, 'COMPSCI 320');
+function printGraph(graph) {
+  for (const edge of graph.edges()) {
+    const v = edge.v.split(" ")[0] + "_" + edge.v.split(" ")[1];
+    const w = edge.w.split(" ")[0] + "_" + edge.w.split(" ")[1];
+    console.log( v + " -> " + w + ";");
+  }
+}
+// printGraph(graph);
+subgraph = makeSubGraph(graph, 'COMPSCI 320');
+printGraph(subgraph);
 
 // console.log(alg.topsort(graph));
 sortedCourses = alg.topsort(graph).map((courseKey) => graph.node(courseKey));
