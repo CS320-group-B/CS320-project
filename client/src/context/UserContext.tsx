aa97bab2fa50bf082cf4277938108b89cc6909fc
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-05-01 21:40:29 -0400

    changed UserContext.tsx to follow UserDataInterface

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,28 +1,50 @@
 import React from 'react'
-import { createContext, useContext, useState} from "react";
+import { createContext } from "react";
+import { getEnrollments, getCourses } from "../api_file/getEnrollments"; // someone is working on this
 
 
-type UserContextType = {
-    userState: number;
-    setUserState: React.Dispatch<React.SetStateAction<number>>;
+
+export const createTrackFromEnrollments = (enrollments:any) => {
+    const track = {semesters: []};
+    enrollments.forEach((enrollment:any) => {
+        // if an object with the matching year and season exists in the array
+        //   access it; else create a new object
+        // use the course key to get the course to get the name of the course?
+        // and add it to the classes array
+
+    });
+
+    return track;
 }
 
-export const UserContext = createContext<UserContextType | null>(null)
+
+export interface UserDataInterface {
+    user: string;
+    track: any;
+  }
+
+export const UserContext = createContext({
+    userData: {} as Partial<UserDataInterface>,
+    setUserData: {} as React.Dispatch<React.SetStateAction<Partial<UserDataInterface>>>,
+});
 
 type Props = {
     children: React.ReactNode
 }
 
 export const UserContextProvider = ({ children }:Props) => {
-    const [userState, setUserState] = React.useState(0);
+    const [userData, setUserData] = React.useState({} as Partial<UserDataInterface>);
     return(
-        <UserContext.Provider value={ {userState, setUserState} }>
+        <UserContext.Provider value={ {userData, setUserData} } >
             {children}
         </UserContext.Provider>
     )
 }
 
 export default UserContext 
+
+
+
 
 
 
