e8167bee95380e58f032d62944c58650c3dc7d5a
danfuz <danryafuz@gmail.com>
2023-05-02 09:01:27 -0400

    MakeFile

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,15 +2,19 @@ const express = require('express');
 const auth = require('../middleware/auth.js');
 const updPassword = require('../middleware/updatePassword.js')
 
-const { getUser, addUser, updateUser, deleteUser, signin, signup, addTaken, removeTaken } = require('../controllers/users.js');
+const { getUser, addUser, updateUser, deleteUser, signin, signup, addTaken, removeTaken, getTaken} = require('../controllers/users.js');
 
 const router = express.Router();
 
 
 router.post('/signin', signin);
 router.post('/signup', signup);
+
 router.get('/', auth, getUser);
+router.get('/taken', auth, getTaken);
+
 router.post('/', auth, addUser);
+
 router.put('/', auth, updPassword, updateUser);
 router.put('/addTaken', auth, addTaken);
 
