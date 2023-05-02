007281ca361a9c2059c89809cb5533f40f8d6b39
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-05-01 22:25:45 -0400

    started createTrackFromEnrollments function

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,16 +1,69 @@
 import React from 'react'
 import { createContext } from "react";
-import { getEnrollments, getCourses } from "../api_file/getEnrollments"; // someone is working on this
+import { getEnrollments, getCourse } from "../api_file/getEnrollments"; // someone is working on this
+import { Semester, Track } from '../constants/Track';
+/*
+const courseSchema = mongoose.Schema({
+    name: String,
+    key: String,
+    credits: Number,
+    number: String,
+    professors: [{"professor_key": String}],
+    subject: String,
+    description: String,
+    prerequisites: [{"prerequisite_key": String}]
+});
+
+const enrollmentSchema = mongoose.Schema({
+    id: String,
+    year: Number,
+    season: String,
+    student_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    course_key: { type: String, required: true },
+    date: Date
+});
+
+export const track: Track = {
+    semesters: [
+        {
+            year: 2022,
+            season: "Fall",
+            classes: [
+                "COMPSCI 220",
+
+                "MATH 235",
+                "COMPSCI 230",
+                "KIN 110"
+            ]
+        },
+    ]
+}
+
+*/
 
 
-
-export const createTrackFromEnrollments = (enrollments:any) => {
-    const track = {semesters: []};
-    enrollments.forEach((enrollment:any) => {
+export const createTrackFromEnrollments = async (enrollments:any) => {
+    const track:Track = {semesters: []};
+    enrollments.forEach(async (enrollment:any) => {
         // if an object with the matching year and season exists in the array
         //   access it; else create a new object
         // use the course key to get the course to get the name of the course?
         // and add it to the classes array
+
+        const year = enrollment.year;
+        const season = enrollment.season;
+        const index = track.semesters.findIndex(x => x.year === year && x.season === season);
+        const className = await getCourse(/* parameters here after api_file is completed */);
+
+        if (index === -1) {
+            track.semesters.push({
+                year: year,
+                season: season,
+                classes: [className]
+            });
+        } else {
+            track.semesters[index].classes.push(className);
+        }
 
     });
 
