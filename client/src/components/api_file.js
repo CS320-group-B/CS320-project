b4a5a992a9f9101bc7fbdab2877ee5e4ace2e782
LeVi4869 <thanhvinhle3900@gmail.com>
2023-05-04 10:15:07 -0300

    added all available APIs

--- client/src/components/api_file.js
+++ client/src/components/api_file.js
@@ -44,11 +44,66 @@ export const login = async (userData) => {
     return result;
 }
 
+export const getUser = async (userData) => {
+    // userData should have an id field
+    const config = {
+        method: "get",
+        url: `${devUrl}/user/${userData.id}`,
+        data: userData
+    }
+
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
 export const updateUser = async (userData) => {
     // userData should have id/newUser
     const config = {
         method: "post",
         url: `${devUrl}/user/${userData.id}`,
+    }
+
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const deleteUser = async (userData) => {
+    // userData should have id
+    const config = {
+        method: "delete",
+        url: `${devUrl}/user/${userData.id}`,
+    }
+
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const addCourse = async (userData) => {
+    // userData should have number field
+    const config = {
+        method: "post",
+        url: `${devUrl}/course`,
         data: userData
     }
 
@@ -66,9 +121,96 @@ export const updateUser = async (userData) => {
 export const getAllCourses = async (userData) => {
     var config = {
         method: 'get',
-        maxBodyLength: Infinity,
-        url: `${devUrl}/course`,
-        headers: { }
+        url: `${devUrl}/course`
+      };
+      
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const getCourse = async (userData) => {
+    // userData should have an id field
+    var config = {
+        method: 'get',
+        url: `${devUrl}/course/${userData.id}`
+      };
+      
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const updateCourse = async (userData) => {
+    // userData should have an id field and optionally the fields you want to update
+    var config = {
+        method: 'put',
+        url: `${devUrl}/course/${userData.id}`,
+        data: userData
+      };
+      
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const deleteCourse = async (userData) => {
+    // userData should have an id field
+    var config = {
+        method: 'delete',
+        url: `${devUrl}/course/${userData.id}`
+      };
+      
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const getAllProfessors = async (userData) => {
+    var config = {
+        method: 'get',
+        url: `${devUrl}/professor`
+      };
+      
+    var result;
+    await axios(config)
+    .then(function (response) {
+        result = response.data
+    })
+    .catch(function (error) {
+        result = error;
+    });
+    return result;
+}
+
+export const getAllSections = async (userData) => {
+    var config = {
+        method: 'get',
+        url: `${devUrl}/section`
       };
       
     var result;
