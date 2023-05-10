e3f5c9fc9aee106c2ad21b414c36f1d75be92e22
Kevin Downing <36209619+v2@users.noreply.github.com>
2023-05-10 19:57:20 -0400

    Reference user context provider instead of mocks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,38 @@
 import Flow from "./components/Track/Flow";
-import { BrowserRouter, Routes, Route, UNSAFE_RouteContext } from "react-router-dom";
+import { BrowserRouter, Routes, Route, UNSAFE_RouteContext, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import Schedule from "./components/Schedule/Schedule";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
-import { UserContextProvider } from './context/UserContext';
+import UserContext, { UserContextProvider } from './context/UserContext';
 
 
 
-export default function App() { 
+export default function App() {
 
   return (
-  <UserContextProvider>  
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/track" element={<Flow />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+    <UserContextProvider>
+      <UserContext.Consumer>
 
-        </Routes>
-      </BrowserRouter>
+        {({ userData, setUserData }) => {
+          console.log(userData);
+
+          return (<BrowserRouter>
+            <Navbar />
+            <Routes >
+              <Route path="/" element={userData ? <Home /> : <Navigate replace to="/login" />} />
+              <Route path="/track" element={userData ? <Flow /> : <Navigate replace to="/login" />} />
+              <Route path="/schedule" element={userData ? <Schedule /> : <Navigate replace to="/login" />} />
+              <Route path="/profile" element={userData ? <Profile /> : <Navigate replace to="/login" />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+
+            </Routes>
+          </BrowserRouter>);
+        }}
+      </UserContext.Consumer>
     </UserContextProvider>
   );
 }
